1d4520ce12d3a1130e918ffe920bb455f8984d0b
pwakefield88 <paul.wakefield@soprasteria.com>
2019-11-07 13:30:49 +0000

    1 file modified, 1 deleted

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
@@ -10,16 +10,22 @@ import { MessageService } from './message.service';
 
 export class HeroService {
 
-  constructor(private messageService: MessageService){}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService){}
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: Fetched heroes');
-    return of(HEROES);
-    }
-
+  getHeroes (): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesUrl)
+  }
     getHero(id: number): Observable<Hero> {
   // TODO: send the message _after_ fetching the hero
-  this.messageService.add(`HeroService: fetched hero id=${id}`);
-  return of(HEROES.find(hero => hero.id === id));
-}
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';
+}
